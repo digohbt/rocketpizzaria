91adce37ffa215e5d226ce0513e28dee221ffcae
Diego Teodoro <digohbt@gmail.com>
2021-04-13 17:15:06 -0300

    finalizado Fat: ModalEditFood

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,92 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+
+interface AddFood {
+  imagem:string;
+  name:string;
+  price:string;
+  description:string;
+}
+interface editingFood {
+  setIsOpen: () => void ; 
+  handleUpdateFood:(data : AddFood) => void ; 
+  isOpen:boolean ;  
+  editingFood :{} ;
+}
+
+
+export default function  ModalEditFood ({setIsOpen, handleUpdateFood, isOpen,  editingFood  }:editingFood) {
+   const formRef = useRef(null)
+  const handleSubmit = async (data : AddFood) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+
+}
+
+// class ModalEditFoodd extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.formRef = createRef()
+//   }
+
+//   handleSubmit = async (data) => {
+//     const { setIsOpen, handleUpdateFood } = this.props;
+
+//     handleUpdateFood(data);
+//     setIsOpen();
+//   };
+
+//   render() {
+//     const { isOpen, setIsOpen, editingFood } = this.props;
+
+//     return (
+//       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+//         <Form ref={this.formRef} onSubmit={this.handleSubmit} initialData={editingFood}>
+//           <h1>Editar Prato</h1>
+//           <Input name="image" placeholder="Cole o link aqui" />
+
+//           <Input name="name" placeholder="Ex: Moda Italiana" />
+//           <Input name="price" placeholder="Ex: 19.90" />
+
+//           <Input name="description" placeholder="Descrição" />
+
+//           <button type="submit" data-testid="edit-food-button">
+//             <div className="text">Editar Prato</div>
+//             <div className="icon">
+//               <FiCheckSquare size={24} />
+//             </div>
+//           </button>
+//         </Form>
+//       </Modal>
+//     );
+//   }
+// };
+
+// export default ModalEditFood;
